5a85722bd7edaaaccc78b36006b91779cf816edf
naeemkhan12 <naeemb7070@gmail.com>
2018-02-17 01:00:07 +0500

    modify user-add to fix permission problems

--- k8s/src/deployment.py
+++ k8s/src/deployment.py
@@ -1,11 +1,13 @@
 from os import path
 import yaml
+# import urllib.request
 from kubernetes import client, config
 def main():
     # Configs can be set in Configuration class directly or using helper
     # utility. If no argument provided, the config will be loaded from
     # default location.
     config.load_kube_config()
+    # response = urllib.request.urlopen()
     with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
         dep = yaml.load(f)
         # v1 = client.CoreV1Api()
