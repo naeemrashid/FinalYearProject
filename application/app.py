38fbeb75a9b53d5ce12e3d9b197a34b511b7edc4
naeemkhan12 <naeemb7070@gmail.com>
2018-03-15 00:03:37 +0500

    home page design

--- application/app.py
+++ application/app.py
@@ -46,7 +46,7 @@ def login_required(test):
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
+    return render_template('pages/home.html')
 
 
 @app.route('/about')
