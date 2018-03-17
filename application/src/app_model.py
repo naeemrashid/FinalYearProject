fae5561595a629d8dc9590405998623caf333621
naeemkhan12 <naeemb7070@gmail.com>
2018-03-18 00:20:43 +0500

    db model for app

---
+++ application/src/app_model.py
@@ -0,0 +1,17 @@
+class App:
+    def __init__(self,title,subtitle,desc,icon,download_url,service,app_url):
+        self._title=title
+        self._subtitle=subtitle
+        self._desc=desc
+        self._icon=icon
+        self._download_url=download_url
+        self._service=service
+        self._app_url=app_url
+    def get_app(self):
+        return {'title':self._title,
+                'subtitle':self._subtitle,
+                'description':self._desc,
+                'icon':self._icon,
+                'download_url':self._download_url,
+                'service':self._service,
+                'app_url':self._app_url}
